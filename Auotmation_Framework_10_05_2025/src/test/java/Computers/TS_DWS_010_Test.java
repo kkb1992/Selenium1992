1d6552045ae635d5af481716783aec76bb411211
kkb1992 <kkinube@gmail.com>
2025-05-16 08:43:52 +0530

    Update TS_DWS_010_Test.java

--- Auotmation_Framework_10_05_2025/src/test/java/Computers/TS_DWS_010_Test.java
+++ Auotmation_Framework_10_05_2025/src/test/java/Computers/TS_DWS_010_Test.java
@@ -16,6 +16,7 @@ public class TS_DWS_010_Test extends BaseClass {
 		hp.getBookslink().click();
 		Assert.assertEquals(driver.getTitle(), "Demo Web Shop. Computers","Computers  Page  is not displayed");
 		test.log( Status.PASS, "Computers  Page  is   displayed");
+		test.log( Status.PASS, "Computers  Page  is   displayed");
 	}
 
 
